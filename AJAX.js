394157a1aaa57ddfcaf4f2247b743f968f84ce95
Drewster317 <drewster317@gmail.com>
2015-11-13 07:52:58 -0500

    MODIFY AJAX

--- AJAX.js
+++ AJAX.js
@@ -6,7 +6,7 @@ var first_name, last_name;
 
 var projApi = {
   gameWatcher: null,
-  url: 'https://secret-cove-5044.herokuapp.com/',
+  url: 'http://localhost:3000',
 
 
   ajax: function(config, cb) {
